ca20c3c8ae2a8e45d04d62910d0e1a5453541156
Barış Savrun <brsvrn@gmail.com>
2026-08-03 15:15:19 +0300

    fix(gtm): put GTM snippet at the absolute top of head and noscript immediately after opening body

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -132,6 +132,18 @@ function RootShell({ children }: { children: ReactNode }) {
   return (
     <html lang="tr">
       <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${gtmId}');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
+
         <HeadContent />
         {/* Google Consent Mode v2 Default Setup */}
         <script
@@ -148,21 +160,6 @@ function RootShell({ children }: { children: ReactNode }) {
             `,
           }}
         />
-
-        {/* Google Tag Manager */}
-        {gtmId && (
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','${gtmId}');
-              `,
-            }}
-          />
-        )}
 
         {/* Google Analytics 4 & Google Ads */}
         {(gaId || googleAdsId) && (
